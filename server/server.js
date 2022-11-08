8533e8e615f7d63a7c98e2cb023ff60a4a3affdd
felikan <felix.herpich@softwareone.com>
2022-11-08 14:50:01 +0100

    Servinator

---
+++ server/server.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser")
+const mongoString = "mongodb+srv://Umrecheninator:AXD1UnkGacxcPC9T@umrecheninator.ftcsi2x.mongodb.net/umrecheninatorUnits";
+
+//database
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+const Model = require("./models/model")
+
+database.on('error', (error) => {
+    console.log(error);
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+
+//server
+const app = express();
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+//app.use('/api', routes);
+app.listen(3000, () => {
+    console.log("Server at 3000");
+})
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+})
+app.get("/getAll", async (req, res) => {
+    try {
+        const units = await Model.find();
+        console.log(units)
+        res.json(units)
+    }
+    catch(error) {
+        res.status(500).json({message: error.message})
+    }
+})
+app.post("/test", async (req, res) => {
+        const unit = new Model({
+        unitName: req.body.unitName,
+        unitSize: req.body.unitSize
+    })
+    
+    try {
+        const unitToAdd = await unit.save();
+        res.status(200).json(unitToAdd);
+    }
+    catch (error) {
+        res.status(400).json({message: error.message})
+    }
+})
