784cb1a335270296238a74ac4f98240ee42bd1a3
Markus Rosenberg <markus.rosenberg@software.com>
2022-11-24 21:41:55 +0100

    Refactored Navbar

---
+++ client/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,71 @@
+import { MouseEventHandler, useRef, useState } from "react";
+import { InputValue } from "../App";
+
+function getOptionsValueByKey(value: string): Options {
+  return Object.entries(Options).find(([key, _]) => key === value)?.[1] as Options;
+}
+
+interface Props {
+  setInput: (input: InputValue) => MouseEventHandler<HTMLButtonElement>;
+}
+
+function Navbar(props: Props) {
+  // TODO: remove styling
+  const customStyles = {
+    control: (base: any, _: any) => ({
+      ...base,
+      height: "34px",
+      minHeight: "34px",
+    }),
+  };
+
+  const inputValueRef = useRef<HTMLInputElement>(null);
+  const [selectValue, setSelectValue] = useState<Options>(Options.m);
+
+  const setSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    e.preventDefault();
+    setSelectValue(getOptionsValueByKey(e.target.value));
+  }
+
+  let disabled = inputValueRef.current?.value ? true : false;
+
+  return (
+    <>
+      <header className="is-flex">
+        <input ref={inputValueRef} type="number" pattern="[0-9]+" id="value" />
+        <select
+          value={selectValue}
+          onChange={e => setSelect(e)}
+          id="dropdown"
+        >
+          {Object.keys(Options).map(x => (
+            <option value={(x)}>{x}</option>
+          ))}
+        </select>
+        <button 
+          onClick={
+            props.setInput({
+              value: parseFloat(inputValueRef.current!.value),
+              size: selectValue
+            })
+          } 
+          id="enter"
+          disabled={disabled}
+        >
+          Erleuchtiniere Mich!
+        </button>
+      </header>
+      <div id="hr">
+        <hr />
+      </div>
+    </>
+  );
+}
+
+enum Options {
+  cm = 0.01,
+  m = 1,
+  km = 1000
+}
+
+export default Navbar;
